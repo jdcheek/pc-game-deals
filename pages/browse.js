d85568c3cf67922a1a177289e0bb5ebcb8fb0595
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-01-17 20:24:00 -0600

    added initial pages and basic links

--- pages/browse.js
+++ pages/browse.js
@@ -2,19 +2,19 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Search() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>PC GAME DEALS</title>
+        <title>PC GAME DEALS - Browse</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>home page</h1>
+      <h1>browse page</h1>
       <Link href='/search'>
         <a>search</a>
       </Link>
-      <Link href='/browse'>
-        <a>browse</a>
+      <Link href='/'>
+        <a>home</a>
       </Link>
     </div>
   );
--- pages/search.js
+++ pages/search.js
@@ -2,16 +2,16 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Search() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>PC GAME DEALS</title>
+        <title>PC GAME DEALS - Search</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>home page</h1>
-      <Link href='/search'>
-        <a>search</a>
+      <h1>search page</h1>
+      <Link href='/'>
+        <a>home</a>
       </Link>
       <Link href='/browse'>
         <a>browse</a>
