6da2ad242fa74effb67e8fd5ca53e35ff63e9e88
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-01-18 21:32:01 -0600

    created nav styles and break points

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import style from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <h3>PC GAME DEALS</h3>
-      <Link href='/'>
-        <a>home</a>
-      </Link>
-      <Link href='/browse'>
-        <a>browse</a>
-      </Link>
-      <Link href='/search'>
-        <a>search</a>
-      </Link>
-      <input type='text' />
+    <div className={style.container}>
+      <div className={style.navbar}>
+        <h1 className={style.title}>PC GAME DEALS</h1>
+        <div className={style.nav}>
+          <Link href='/'>
+            <a>home</a>
+          </Link>
+          <Link href='/browse'>
+            <a>browse</a>
+          </Link>
+          <Link href='/search'>
+            <a>search</a>
+          </Link>
+          <input className={style.input} type='text' />
+        </div>
+        <div className={style.burger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 }
