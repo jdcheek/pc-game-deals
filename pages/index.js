6551214e15540dec72057824ecb8686bd0cd5915
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-01-19 21:07:55 -0600

    ssr function for home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
+import GameCard from "../components/GameCard";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <Head>
@@ -11,7 +12,20 @@ export default function Home() {
       <Navbar />
       <div className='page-container'>
         <h1>home page</h1>
+        <div>
+          <GameCard games={props.games} />
+        </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    `https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15&AAA=1`
+  );
+  const data = await res.json();
+  return {
+    props: { games: data },
+  };
 }
