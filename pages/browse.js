13b8b9d57d1e6f477e969702573b3df474404c52
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-01-27 23:33:26 -0600

    implemented temporary client side infinite scrolling

--- pages/browse.js
+++ pages/browse.js
@@ -1,9 +1,42 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import GameCard from "../components/GameCard";
 import style from "../styles/Browse.module.css";
 
-export default function Browse(props) {
+export default function Browse({ games }) {
+  const [loading, setLoading] = useState(false);
+  const [gameResults, setGameResults] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const fetchNextPage = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_FETCH_URL}&upperPrice=15&pageNumber=${page}&pageSize=10`
+      );
+      if (res.status !== 200) {
+        return console.log("Failed to fetch" + res.status);
+      }
+      games = await res.json();
+      if (games.length > 0) {
+        setGameResults(gameResults.concat(games));
+        setPage(page + 1);
+      }
+    } catch (err) {
+      games = { error: { message: err.message } };
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setGameResults(games);
+  }, []);
+
+  useEffect(() => {
+    setPage(page + 1);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -13,20 +46,30 @@ export default function Browse(props) {
       <div className='nav-buffer'></div>
       <div className='page-container'>
         <h1 className={style.title}>BROWSE TITLES</h1>
-        <GameCard games={props.games} />
+        <GameCard games={gameResults} fetchNextPage={fetchNextPage} />
+        {loading && (
+          <div className={"loader"}>
+            <p>Loading...</p>
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
-export async function getServerSideProps() {
+export const getServerSideProps = async () => {
+  let games = null;
+
   try {
-    const res = await fetch(`${process.env.FETCH_URL}&upperPrice=15`);
-    const data = await res.json();
-    return {
-      props: { games: data },
-    };
-  } catch (error) {
-    console.log(error);
+    const res = await fetch(
+      `${process.env.FETCH_URL}&upperPrice=15&pageNumber=0&pageSize=10`
+    );
+    if (res.status !== 200) {
+      return console.log("Failed to fetch" + res.status);
+    }
+    games = await res.json();
+  } catch (err) {
+    games = { error: { message: err.message } };
   }
-}
+  return { props: { games } };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,45 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import style from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import GameCard from "../components/GameCard";
 
-export default function Home({ games, curPage }) {
+export default function Home({ games }) {
+  const [loading, setLoading] = useState(false);
+  const [gameResults, setGameResults] = useState([]);
+  const [page, setPage] = useState(0);
+  const [end, setEnd] = useState(false);
+
+  const fetchNextPage = async () => {
+    end ? setLoading(false) : setLoading(true);
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_FETCH_URL}&upperPrice=15&AAA=1&pageNumber=${page}&pageSize=10`
+      );
+      if (res.status !== 200) {
+        return console.log("Failed to fetch" + res.status);
+      }
+      games = await res.json();
+      if (games.length > 0) {
+        setGameResults(gameResults.concat(games));
+        setPage(page + 1);
+      } else {
+        setEnd(true);
+      }
+    } catch (err) {
+      games = { error: { message: err.message } };
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setGameResults(games);
+  }, []);
+
+  useEffect(() => {
+    setPage(page + 1);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -13,20 +49,23 @@ export default function Home({ games, curPage }) {
       <div className='nav-buffer'></div>
       <div className='page-container'>
         <h1 className={style.title}>TOP AAA DEALS</h1>
-        <GameCard games={games} curPage={curPage} />
+        <GameCard games={gameResults} fetchNextPage={fetchNextPage} />
+        {loading && (
+          <div className={"loader"}>
+            <p>Loading...</p>
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
-export const getServerSideProps = async ({ query }) => {
-  let page = query.page || 0;
+export const getServerSideProps = async () => {
   let games = null;
-  const curPage = page;
 
   try {
     const res = await fetch(
-      `${process.env.FETCH_URL}&upperPrice=15&AAA=1&pageNumber=${page}&pageSize=20`
+      `${process.env.FETCH_URL}&upperPrice=15&AAA=1&pageNumber=0&pageSize=10`
     );
     if (res.status !== 200) {
       return console.log("Failed to fetch" + res.status);
@@ -35,5 +74,5 @@ export const getServerSideProps = async ({ query }) => {
   } catch (err) {
     games = { error: { message: err.message } };
   }
-  return { props: { games, curPage } };
+  return { props: { games } };
 };
