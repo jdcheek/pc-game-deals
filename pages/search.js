d0c065a91aa0db831b8ee6fbb2832b918816f63b
jdcheek <58921502+jdcheek@users.noreply.github.com>
2021-02-07 10:58:55 -0600

    removed comments

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import Navbar from "../components/Navbar";
 import GameCard from "../components/GameCard";
 import TopButton from "../components/TopButton";
 import SortPopup from "../components/SortPopup";
